30cee9bbd0bdbba556e7090df353b42a5187c080
jose <jose@Beta>
2020-11-18 20:29:47 +0100

    Borrar Plantilla

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="search" action="<?php bloginfo('url'); ?>/">
-	<div>
-		<input type="text" value="<?php the_search_query(); ?>" name="s" id="search-text" />
-		<input type="submit" id="search-submit" value="" />
-	</div>
-</form>
